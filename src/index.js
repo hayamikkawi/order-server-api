c2dbc6eee135e56be728093e2a195f068d00ce11
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2022-03-27 20:31:50 +0300

    refactored the project (added routes directory)

--- src/index.js
+++ src/index.js
@@ -2,51 +2,14 @@ require('./db/mongoose') //so that this runs
 const express = require('express')
 const Book = require('./models/book.js')
 const request = require('request') 
+const bookRouter = require('./routes/book')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 app.use(express.json()) //to parse incoming json to object.
+app.use(bookRouter)
 
-//purchase a book
-app.get('/books/purchase/:id', async(req, res1)=>{
-    const id = req.params.id
-    const url = 'http://127.0.0.1:3000/books/info/'+id
-    console.log(url)
-    request({url, json:true}, (req, res)=>{
-        console.log(res.body)
-        if(res.statusCode ==404){
-            res1.status(404).send(res.body)
-        }
-        const count = res.body.numberOfItems
-        if(count<=0){
-             res1.status(404).send("This item is out of stock")
-        }
-        else{
-             const decUrl = 'http://127.0.0.1:3000/books/count/'+id
-             console.log(decUrl)
-             request.patch(
-                 //First parameter API to make post request
-                 decUrl,
-
-                //The second parameter, DATA which has to be sent to API
-                { json: {
-                    numberOfItems: count-1,
-                    } 
-                },
-    
-                 //The third parameter is a Callback function 
-                (req, res2)=>{
-                    if(res2.statusCode == 200){
-                        res1.send("item purchased successfully")
-                    }
-                    else {res1.send(res2)}
-                    
-                }
-            );
-        }
-   })
-})
 
 app.listen(port, ()=>{
     console.log('server is listening')
---
+++ src/routes/book.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const request = require('request')
+
+const router = express.Router()
+// check book 
+const checkBooks = (id)=>{
+    return new Promise((resolve, reject)=>{
+        const url = 'http://127.0.0.1:3000/books/info/'+id
+        request({url, json:true}, (error, res, body)=>{
+            if(error){
+                return reject(error)
+            } else if(res.statusCode ==404){
+                return reject(body)
+            }  
+            const count = body.numberOfItems
+            if(count<=0){
+                 return reject("This item is out of stock")
+            }
+            console.log('hey')
+            return resolve({msg:"available", count})
+
+        })
+    })
+}
+//buy book
+const buyBook = (id,count)=>{
+    return new Promise((resolve, reject)=>{
+        const decUrl = 'http://127.0.0.1:3000/books/count/'+id
+        request.patch(
+            decUrl,
+            {json:{numberOfItems: count-1}},
+            (error, res)=>{ 
+                if(error){
+                    return reject(error)
+                }else if(res.statusCode == 200){
+                    return resolve("item purchased successfully")
+                            }
+                        })  
+    }) 
+}
+//purchase a book
+router.get('/books/purchase/:id', async(req, res)=>{
+    try{
+        const id = req.params.id
+        const checked = await checkBooks(id)
+        console.log(checked)
+        const buy = await buyBook(id, checked.count)
+        console.log(buy)
+        res.status(200).send(buy)
+    } catch(error){
+      res.status(404).send(error)
+    }
+})
+module.exports = router
